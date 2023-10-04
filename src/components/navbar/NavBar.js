49b5158b97b71b3c2a068c3072fb7963476f9681
Heine Günther <heine_gunther@hotmail.com>
2023-10-04 20:43:18 +0200

    Update NavBar.js

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,28 +1,13 @@
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Container from "@mui/material/Container";
 import Logo from "./logo/logo.png";
 
 function ResponsiveAppBar() {
   return (
-    <AppBar
-      position="static"
-      sx={{
-        backgroundColor: "inherit",
-        boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.2)",
-      }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <img
-            src={Logo}
-            alt="logo"
-            style={{ height: "35px", margin: "auto" }}
-          />
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <nav>
+      <div>
+        <img src={Logo} alt="logo" style={{ height: "35px", margin: "auto" }} />
+      </div>
+    </nav>
   );
 }
 export default ResponsiveAppBar;
